a55ebe9b7cd8f7ddb87a42bf29e187f2604c37d6
Derek Peacock <derek.peacock@yahoo.com>
2020-11-30 14:15:56 +0000

    Initial commit

---
+++ src/Map.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+/**
+ *  This is a comment
+ */
+public class Map
+{
+    private ArrayList<Room> room;
+
+
+    public void print()
+    {
+
+    }
+
+    public Map()
+    {
+        room = new ArrayList<Room>();
+    }
+}
